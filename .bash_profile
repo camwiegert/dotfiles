a81cecf06097211e3b7a345f3242214da74f0b14
Cam Wiegert <cam@camwiegert.com>
2016-05-30 16:18:56 -0500

    adds functions

--- .bash_profile
+++ .bash_profile
@@ -7,7 +7,7 @@ export PATH="$HOME/.bin:$PATH"
 export PATH="$HOME/.bin/lib/node/bin:$PATH"
 
 # Load some other files, if they exist.
-for file in ~/.{aliases,bash-completion.bash}; do
+for file in ~/.{aliases,functions,git-completion.bash}; do
     [ -f "$file" ] && source "$file"
 done
 unset file
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Symlink our dotfiles into ~ interactively. Has to
 # be run from within the dotfiles directory.
 
-for file in .{aliases,bash_profile,gitconfig,gitignore,hushlogin,inputrc,npmrc}; do
+for file in .{aliases,functions,bash_profile,gitconfig,gitignore,hushlogin,inputrc,npmrc}; do
   ln -i -s "$(pwd)/$file" ~/"$file"
 done
 unset file
