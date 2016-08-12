140c07bc1eb75ad87a18e8ded522d3ae9026878d
Cam Wiegert <cam@camwiegert.com>
2016-08-11 22:12:09 -0500

    add hyperterm config to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Symlink our dotfiles into ~ interactively. Has to
 # be run from within the dotfiles directory.
 
-for file in .{aliases,functions,bash_profile,bashrc,gitconfig,gitignore,hushlogin,inputrc,npmrc}; do
+for file in .{aliases,functions,bash_profile,bashrc,gitconfig,gitignore,hushlogin,hyperterm.js,inputrc,npmrc}; do
   ln -i -s "$(pwd)/$file" ~/"$file"
 done
 unset file
