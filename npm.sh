ffb0ae4793e4939d97100bba8847ce1fe3b00f04
Cam Wiegert <cam@camwiegert.com>
2016-08-14 09:53:58 -0500

    Add npm bootstrap script

---
+++ npm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+npm install -g clif
+npm install -g http-server
+npm install -g json-server
+npm install -g node-sass
+npm install -g nodemon
+npm install -g parker
+npm install -g webpack
