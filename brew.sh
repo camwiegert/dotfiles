e2917de0fe91ce3e3c64e717fcd13e1fdd700399
Cam Wiegert <cam@camwiegert.com>
2016-12-19 10:06:57 -0600

    Add cask installs to brew.sh

--- brew.sh
+++ brew.sh
@@ -14,7 +14,25 @@ brew install neofetch
 brew install nmap
 brew install openssl
 brew install tree
+brew install vim
 brew install wget
+
+# Install some apps.
+brew cask install 1password
+brew cask install atom
+brew cask install dropbox
+brew cask install flux
+brew cask install google-chrome
+brew cask install hyper
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install slack
+brew cask install spectacle
+brew cask install spotify
+brew cask install transmission
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
 
 # Remove outdated packages from the cellar.
 brew cleanup
