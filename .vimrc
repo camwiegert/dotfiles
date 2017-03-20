92387f299939079774e7d8f03440d9c605cceb61
Cam Wiegert <cam@camwiegert.com>
2017-03-20 10:47:22 -0500

    Add basic vimrc

---
+++ .vimrc
@@ -0,0 +1,8 @@
+set number
+set showmatch
+set wildmenu
+
+set expandtab
+set softtabstop=2
+set tabstop=2
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Symlink our dotfiles into ~ interactively. Has to
 # be run from within the dotfiles directory.
 
-for file in .{aliases,functions,bash_profile,bashrc,gitconfig,gitignore,hushlogin,hyper.js,inputrc,npmrc,ackrc}; do
+for file in .{aliases,functions,bash_profile,bashrc,gitconfig,gitignore,hushlogin,hyper.js,inputrc,npmrc,ackrc,vimrc}; do
   ln -i -s "$(pwd)/$file" ~/"$file"
 done
 unset file
