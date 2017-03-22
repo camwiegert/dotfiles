a092b7e3fde7409c90e115f06e0640d4bca26cc9
Cam Wiegert <cam@camwiegert.com>
2017-03-22 11:59:36 -0500

    Move npm.sh -> yarn.sh

---
+++ yarn.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+yarn global add fast-cli
+yarn global add gulp
+yarn global add serve
+yarn global add json-server
+yarn global add node-sass
+yarn global add nodemon
+yarn global add parker
+yarn global add svgo
+yarn global add webpack
