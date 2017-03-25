a09b3f5cf708e431e822d79ac7513a1817e6abb5
Cam Wiegert <cam@camwiegert.com>
2017-03-24 20:13:28 -0500

    Disable bell in hyper config

--- .hyper.js
+++ .hyper.js
@@ -3,7 +3,8 @@ module.exports = {
   config: {
     fontFamily: '"SF Mono", "Menlo", "DejaVu Sans Mono", "Lucida Console", monospace',
     fontSize: 13,
-    windowSize: [1080, 640]
+    windowSize: [1080, 640],
+    bell: false
   },
 
   plugins: [
