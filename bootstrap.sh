dd2b0d130941a84fcca9aa527b40db6dd566cc4a
Cam Wiegert <cam@camwiegert.com>
2017-06-25 21:36:29 -0500

    Prefer npm over yarn for globals

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,4 @@ curl -o ~/.git-completion.bash \
 https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 source ./brew.sh
-source ./yarn.sh
+source ./npm.sh
---
+++ npm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+npm i -g \
+fast-cli \
+gulp \
+json-server \
+node-sass \
+nodemon \
+parker \
+prettier \
+serve \
+svgo \
+webpack
