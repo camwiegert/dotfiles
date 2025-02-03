2738f57fffd85aeb59d0d161a4adc3dca06b59d9
Cam Wiegert <cam@camw.xyz>
2025-02-03 10:00:17 -0600

    Move ms_to_readable into prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -14,12 +14,12 @@ function fish_prompt
 
 	if test $CMD_DURATION -gt 1000
 		set_color yellow
-		printf " %s" (ms_to_duration $CMD_DURATION)
+		printf " %s" (ms_to_readable $CMD_DURATION)
 	end
 
 	if test $last_status -ne 0
 		set_color red
-		printf " ✖"
+		printf " ✘"
 	end
 
 	if test $fish_bind_mode != insert
@@ -29,4 +29,18 @@ function fish_prompt
 
 	set_color normal
 	printf "\n%s " 🐠
+end
+
+function ms_to_readable
+	set -l sec (math floor $argv[1] / 1000)
+	set -l min (math floor $sec / 60)
+	set -l hrs (math floor $min / 60)
+
+	if test $hrs -gt 0
+		printf "%sh %sm %ss" $hrs (math floor $min % 60) (math floor $sec % 60)
+	else if test $min -gt 0
+		printf "%sm %ss" $min (math floor $sec % 60)
+	else
+		printf "%ss" $sec
+	end
 end
